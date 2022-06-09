5eee7e576300dadd2660067034409453475341ae
Maxim Ermilov <max-ermilov@yandex.ru>
2022-06-09 17:27:46 +0300

    add global 404 response

--- app.js
+++ app.js
@@ -8,27 +8,21 @@ const { PORT = 3000 } = process.env;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/mestodb', { family: 4 })
+mongoose.connect('mongodb://localhost:27017/mestodb', { family: 4 }) // { family: 4 } - forces mongoose to use IPv4 instead of IPv6
   .catch((err) => {
     console.log('Не удалось подключиться к базе данных. Ошибка: ', err);
   });
+
 app.use((req, res, next) => {
   req.user = {
     _id: '6299f24405d0fd675eeba048', // hardcoded userId
   };
-
   next();
 });
+
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
-// app.use((req, res, next) => {
-//   req.user = {
-//     _id: '5d8b8592978f8bd833ca8133',
-//   };
-//
-//   next();
-// });
+app.use('*', (req, res) => res.status(404).send({ message: 'Запрашиваемая страница не найдена' }));
 
-// app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(PORT);

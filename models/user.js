e7c186aa799aa481ffb742b9fac36ab98af058f8
Maxim Ermilov <max-ermilov@yandex.ru>
2022-06-17 12:34:10 +0300

    add email n password to schema, edit list of required fields

--- models/user.js
+++ models/user.js
@@ -1,21 +1,39 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (v) => validator.isEmail(v),
+      message: 'Недопустимый формат адреса почты',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+  },
   name: {
     type: String,
-    required: [true, '{PATH} - обязательное поле. '],
     minlength: [2, 'Минимальная длина свойства {PATH} должна быть 2. Передано {VALUE}. '],
     maxlength: [30, 'Максимальная длина свойства {PATH} должна быть 30. Передано {VALUE}. '],
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    required: [true, '{PATH} - обязательное поле. '],
     minlength: [2, 'Минимальная длина свойства {PATH} должна быть 2. Передано {VALUE}. '],
     maxlength: [30, 'Максимальная длина свойства {PATH} должна быть 30. Передано {VALUE}. '],
+    default: 'Исследователь',
   },
   avatar: {
     type: String,
-    required: [true, '{PATH} - обязательное поле. '],
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Недопустимый формат ссылки',
+    },
   },
 });
 

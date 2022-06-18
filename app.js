8c4f001e523723d3d7d556280aaefdd6c70c6bdd
Maxim Ermilov <max-ermilov@yandex.ru>
2022-06-18 19:56:17 +0300

    add auth middleware, add centralised err creation

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
+const { NotFound } = require('./errors');
 const { login, createUser } = require('./controllers/users');
+const auth = require('./middlewares/auth');
 
 const app = express();
 const { PORT = 3000 } = process.env;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 mongoose.connect('mongodb://localhost:27017/mestodb', { family: 4 }) // { family: 4 } - forces mongoose to use IPv4 instead of IPv6
   .catch((err) => {
@@ -18,17 +22,26 @@ mongoose.connect('mongodb://localhost:27017/mestodb', { family: 4 }) // { family
 app.post('/signin', login);
 app.post('/signup', createUser);
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '6299f24405d0fd675eeba048', // hardcoded userId
-  };
-  next();
-});
+// app.use((req, res, next) => {
+//   req.user = {
+//     _id: '6299f24405d0fd675eeba048', // hardcoded userId
+//   };
+//   next();
+// });
+
+app.use(auth);
 
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
-app.use('*', (req, res) => res.status(404).send({ message: 'Запрашиваемая страница не найдена' }));
+app.use('*', (req, res, next) => next(new NotFound('Запрашиваемая страница не найдена')));
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+
+  res.status(statusCode).send({ message: statusCode === 500 ? 'Ошибка сервера' : message });
+  next();
+});
 
 app.listen(PORT, () => {
   console.log(`Сервер успешно запущен на порте ${PORT}`);
--- controllers/users.js
+++ controllers/users.js
@@ -2,13 +2,19 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
+const {
+  BadRequest, Unauthorized, NotFound, Conflict,
+} = require('../errors');
 
-module.exports.login = (req, res) => {
+function prepareValidationMessage(err) {
+  return `Ошибка валидации: ${Object.values(err.errors).map((error) => error.properties.message).join('')}`;
+}
+
+module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
 
   User.findUserByCredentials(email, password)
     .then((user) => {
-      console.log('аутентификация успешна');
       const token = jwt.sign({ _id: user._id }, 'super-strong-secret', { expiresIn: '7d' });
       return res.status(200).cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
@@ -16,40 +22,35 @@ module.exports.login = (req, res) => {
         sameSite: true,
       }).end();
     })
-    .catch((err) => {
-      if (err.message === 'Неправильные почта или пароль') {
-        return res.status(401).send(err.message);
-      }
-      return res.status(500).send({ message: 'Ошибка сервера' });
-    });
+    .catch(() => next(new Unauthorized('Неверные почта или пароль')));
 };
 
-module.exports.getAllUsers = (req, res) => {
+module.exports.getAllUsers = (req, res, next) => {
   User.find({})
     .then((users) => {
       res.status(200).send({ data: users });
     })
-    .catch(() => res.status(500).send({ message: 'Ошибка сервера' }));
+    .catch((err) => next(err));
 };
 
-module.exports.getUserById = (req, res) => {
+module.exports.getUserById = (req, res, next) => {
   const userID = req.params.userId;
   User.findById(userID)
     .then((user) => {
       if (!user) {
-        return res.status(404).send({ message: 'Пользователь не найден' });
+        throw new NotFound('Пользователь не найден');
       }
       return res.status(200).send({ user });
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        return res.status(400).send({ message: 'Некорректный формат id' });
+        return next(new BadRequest('Некорректный формат id'));
       }
-      return res.status(500).send({ message: 'Ошибка сервера' });
+      return next(err);
     });
 };
 
-module.exports.createUser = (req, res) => {
+module.exports.createUser = (req, res, next) => {
   const {
     email, password, name, about, avatar,
   } = req.body;
@@ -61,47 +62,46 @@ module.exports.createUser = (req, res) => {
     .then((user) => res.status(201).send({ data: user }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        const errorMessage = Object.values(err.errors).map((error) => error.properties.message).join('');
-        return res.status(400).send({ message: `Ошибка валидации: ${errorMessage}` });
+        return next(new BadRequest(prepareValidationMessage(err)));
+      } if (err.code === 11000) {
+        return next(new Conflict('Данный email уже занят'));
       }
-      return res.status(500).send({ message: 'Ошибка сервера' });
+      return next(err);
     });
 };
 
-module.exports.updateUser = (req, res) => {
+module.exports.updateUser = (req, res, next) => {
   const { name, about } = req.body;
 
   User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
     .then((user) => {
       if (!user) {
-        return res.status(404).send({ message: 'Пользователь не найден' });
+        throw new NotFound('Пользователь не найден');
       }
       return res.status(200).send({ data: user });
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        const errorMessage = Object.values(err.errors).map((error) => error.properties.message).join('');
-        return res.status(400).send({ message: `Ошибка валидации: ${errorMessage}` });
+        return next(new BadRequest(prepareValidationMessage(err)));
       }
-      return res.status(500).send({ message: 'Ошибка сервера' });
+      return next(err);
     });
 };
 
-module.exports.updateUserAvatar = (req, res) => {
+module.exports.updateUserAvatar = (req, res, next) => {
   const { avatar } = req.body;
 
   User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
     .then((user) => {
       if (!user) {
-        return res.status(404).send({ message: 'Пользователь не найден' });
+        throw new NotFound('Пользователь не найден');
       }
       return res.status(200).send({ data: user });
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        const errorMessage = Object.values(err.errors).map((error) => error.properties.message).join('');
-        return res.status(400).send({ message: `Ошибка валидации: ${errorMessage}` });
+        return next(new BadRequest(prepareValidationMessage(err)));
       }
-      return res.status(500).send({ message: 'Ошибка сервера' });
+      return next(err);
     });
 };

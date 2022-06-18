4babfd1c0a5fbd7673fc3e88742757a9ed00783b
Maxim Ermilov <max-ermilov@yandex.ru>
2022-06-18 22:12:07 +0300

    add req validation using celebrate

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const { errors, celebrate, Joi } = require('celebrate');
 
 const { NotFound } = require('./errors');
 const { login, createUser } = require('./controllers/users');
@@ -19,8 +20,21 @@ mongoose.connect('mongodb://localhost:27017/mestodb', { family: 4 }) // { family
     console.log('Не удалось подключиться к базе данных. Ошибка: ', err);
   });
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(/^https?:\/\/(www\.)?[a-zA-Z\d-]+\.[\w\d\-.~:/?#[\]@!$&'()*+,;=]{2,}#?$/),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), createUser);
 
 app.use(auth);
 
@@ -28,6 +42,8 @@ app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
 app.use('*', (req, res, next) => next(new NotFound('Запрашиваемая страница не найдена')));
+
+app.use(errors());
 
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
